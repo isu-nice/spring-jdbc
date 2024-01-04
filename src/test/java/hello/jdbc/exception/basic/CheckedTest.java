ad6936dfa8f080a2cdba8ba3c38ce1934abbc623
isu-nice <isu010614@naver.com>
2024-01-04 18:57:27 +0900

    test: checked 예외 테스트 (throw)

--- src/test/java/hello/jdbc/exception/basic/CheckedTest.java
+++ src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -3,6 +3,8 @@ package hello.jdbc.exception.basic;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 @Slf4j
 class CheckedTest {
 
@@ -10,6 +12,13 @@ class CheckedTest {
     void checked_catch() {
         Service service = new Service();
         service.callCatch();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(() -> service.callThrow())
+                .isInstanceOf(MyCheckedException.class);
     }
 
     /**
@@ -38,6 +47,16 @@ class CheckedTest {
                 // 예외 처리 로직
                 log.info("예외 처리, message={}", e.getMessage(), e);
             }
+        }
+
+        /**
+         * Checked 예외를 밖으로 던지는 코드
+         * checked 예외는 예외를 잡지 않고 밖으로 던지려면 throws 예외를 메서드에 필수로 선언해야 한다.
+         *
+         * @throws MyCheckedException
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
         }
     }
 
