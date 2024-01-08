5e83690658b63d91dd99b5e8275292763428fc3e
isu-nice <isu010614@naver.com>
2024-01-08 18:03:37 +0900

    chore: SQLException 추가

--- src/main/java/hello/jdbc/repository/MemberRepositoryEx.java
+++ src/main/java/hello/jdbc/repository/MemberRepositoryEx.java
@@ -9,7 +9,7 @@ public interface MemberRepositoryEx {
 
     Member findById(String memberId) throws SQLException;
 
-    void update(String memberId, int money);
+    void update(String memberId, int money) throws SQLException;
 
-    void delete(String memberId);
+    void delete(String memberId) throws SQLException;
 }
