5bd59ac3c8a859099cb31f4654366afb4e1a51d0
isu-nice <isu010614@naver.com>
2024-01-12 01:44:19 +0900

    chore: 리포지토리 인터페이스 변경

--- src/test/java/hello/jdbc/service/MemberServiceV4Test.java
+++ src/test/java/hello/jdbc/service/MemberServiceV4Test.java
@@ -2,7 +2,7 @@ package hello.jdbc.service;
 
 import hello.jdbc.domain.Member;
 import hello.jdbc.repository.MemberRepository;
-import hello.jdbc.repository.MemberRepositoryV4_2;
+import hello.jdbc.repository.MemberRepositoryV5;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.DisplayName;
@@ -50,7 +50,8 @@ class MemberServiceV4Test {
         MemberRepository memberRepository() {
             // MemberRepository(인터페이스)에 대한 구현체로 MemberRepositoryV4_1를 등록한다.
 //            return new MemberRepositoryV4_1(dataSource);
-            return new MemberRepositoryV4_2(dataSource);
+//            return new MemberRepositoryV4_2(dataSource);
+            return new MemberRepositoryV5(dataSource);
         }
 
         @Bean
