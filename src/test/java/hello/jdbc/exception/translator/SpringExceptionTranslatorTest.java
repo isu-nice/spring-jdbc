2c58243814cb3d8741873330385a10b7775800eb
isu-nice <isu010614@naver.com>
2024-01-13 12:20:32 +0900

    test: 스프링 예외 자동 변환 테스트

--- src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
+++ src/test/java/hello/jdbc/exception/translator/SpringExceptionTranslatorTest.java
@@ -3,7 +3,10 @@ package hello.jdbc.exception.translator;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.BadSqlGrammarException;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.support.SQLErrorCodeSQLExceptionTranslator;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
@@ -36,6 +39,24 @@ public class SpringExceptionTranslatorTest {
             int errorCode = e.getErrorCode();
             log.info("errorCode={}", errorCode);
             log.info("error", e);
+        }
+    }
+
+    @Test
+    void exceptionTranslator() {
+        String sql = "select bad grammar";
+
+        try {
+            Connection con = dataSource.getConnection();
+            PreparedStatement pstmt = con.prepareStatement(sql);
+            pstmt.executeQuery();
+        } catch (SQLException e) {
+            assertThat(e.getErrorCode()).isEqualTo(42122);
+
+            SQLErrorCodeSQLExceptionTranslator exTranslator = new SQLErrorCodeSQLExceptionTranslator();
+            DataAccessException resultEx = exTranslator.translate("select", sql, e);
+            log.info("resultEx", resultEx);
+            assertThat(resultEx.getClass()).isEqualTo(BadSqlGrammarException.class);
         }
     }
 }
